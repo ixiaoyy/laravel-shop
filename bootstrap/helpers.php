0fe555870120bb57c1f4678b49c07cf92c606d58
xiaoyz <phpxiaoyz@163.com>
2020-05-08 08:23:43 +0000

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,4 +1,11 @@
 <?php
-function test_helper() {
-    return 'OK';
+
+/**
+  * Desc: 将请求的路由名称转换为 CSS 类名称
+  * User: YuY
+  * Date: 2020/5/8
+  */
+function route_class()
+{
+    return str_replace('.','-',Route::currentRouteName());
 }
