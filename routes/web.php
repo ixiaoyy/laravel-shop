ebc738a68f94448056e2660b999d5b7d2c79a8b2
xiaoyz <phpxiaoyz@163.com>
2020-05-09 10:01:17 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 
-Auth::routes();
+// 在之前的路由里加上一个 verify 参数
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
