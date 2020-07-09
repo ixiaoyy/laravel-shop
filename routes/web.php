96693c1ee964ff858738515e55173b5aea1195cb
vagrant <vagrant@homestead>
2020-07-09 12:44:41 +0000

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
-// 测试中间件
-Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', 'products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 // 在之前的路由里加上一个 verify 参数
 Auth::routes(['verify' => true]);
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
