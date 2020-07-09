e5a3210425a41f4aa585091b4516097bb328e430
ixiaoyy <phpxiaoyz@163.com>
2020-07-09 19:52:53 +0800

    管理后台 - 新增、编辑商品

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 use function foo\func;
 
@@ -25,12 +26,12 @@ class ProductsController extends AdminController
      */
     protected function grid()
     {
-        $grid = new Grid(new Product());
+        $grid = new Grid(new Product);
 
         $grid->id('ID')->sortable();
         $grid->title('商品名称');
         $grid->on_sale('已上架')->display(function ($value) {
-           return $value ? '是' : '否';
+            return $value ? '是' : '否';
         });
         $grid->price('价格');
         $grid->rating('评分');
@@ -38,11 +39,11 @@ class ProductsController extends AdminController
         $grid->review_count('评论数');
 
         $grid->actions(function ($actions) {
-           $actions->disableView();
-           $actions->disableDelete();
+            $actions->disableView();
+            $actions->disableDelete();
         });
         $grid->tools(function ($tools) {
-           // 禁用批量删除按钮
+            // 禁用批量删除按钮
             $tools->batch(function ($batch) {
                 $batch->disableDelete();
             });
@@ -52,47 +53,38 @@ class ProductsController extends AdminController
     }
 
     /**
-     * Make a show builder.
-     *
-     * @param mixed $id
-     * @return Show
-     */
-    protected function detail($id)
-    {
-        $show = new Show(Product::findOrFail($id));
-
-        $show->field('id', __('Id'));
-        $show->field('title', __('Title'));
-        $show->field('description', __('Description'));
-        $show->field('image', __('Image'));
-        $show->field('on_sale', __('On sale'));
-        $show->field('rating', __('Rating'));
-        $show->field('sold_count', __('Sold count'));
-        $show->field('review_count', __('Review count'));
-        $show->field('price', __('Price'));
-        $show->field('created_at', __('Created at'));
-        $show->field('updated_at', __('Updated at'));
-
-        return $show;
-    }
-
-    /**
      * Make a form builder.
      *
      * @return Form
      */
     protected function form()
     {
-        $form = new Form(new Product());
+        $form = new Form(new Product);
 
-        $form->text('title', __('Title'));
-        $form->textarea('description', __('Description'));
-        $form->image('image', __('Image'));
-        $form->switch('on_sale', __('On sale'))->default(1);
-        $form->decimal('rating', __('Rating'))->default(5.00);
-        $form->number('sold_count', __('Sold count'));
-        $form->number('review_count', __('Review count'));
-        $form->decimal('price', __('Price'));
+        // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
+        $form->text('title', '商品名称')->rules('required');
+
+        // 创建一个选择图片的框
+        $form->image('image', '封面图片')->rules('required|image');
+
+        // 创建一个富文本编辑器
+        $form->quill('description', '商品描述')->rules('required');
+
+        // 创建一组单选框
+        $form->radio('on_sale', '上架')->options(['1' => '是', '0'=> '否'])->default('0');
+
+        // 直接添加一对多的关联模型
+        $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
+            $form->text('title', 'SKU 名称')->rules('required');
+            $form->text('description', 'SKU 描述')->rules('required');
+            $form->text('price', '单价')->rules('required|numeric|min:0.01');
+            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+        });
+
+        // 定义事件回调，当模型即将保存时会触发这个回调
+        $form->saving(function (Form $form) {
+            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+        });
 
         return $form;
     }
