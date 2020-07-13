b7918fbafbb986117284e302bf3cc8e0c3eed470
ixiaoyy <phpxiaoyz@163.com>
2020-07-13 20:13:51 +0800

    用户界面- 订单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -12,6 +12,8 @@ use App\Exceptions\InvalidRequestException;
 
 use App\Jobs\CloseOrder;
 
+use Illuminate\Http\Request;
+
 class OrdersController extends Controller
 {
     public function store(OrderRequest $request)
@@ -70,5 +72,17 @@ class OrdersController extends Controller
         $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
         return $order;
+    }
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N+1问题
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
     }
 }
