58d17fbfa245a0772f28f042dd3ee31b9c701827
ixiaoyy <phpxiaoyz@163.com>
2020-07-13 10:29:04 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 Route::redirect('/', 'products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 // 在之前的路由里加上一个 verify 参数
 Auth::routes(['verify' => true]);
@@ -18,4 +17,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
