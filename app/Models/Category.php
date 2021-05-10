4da61cc9988a8762c59fa2150d79d6a2a32057d5
lijin <phpxiaoyz@163.com>
2021-05-10 15:34:09 +0800

    初始化类目模型

---
+++ app/Models/Category.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use function foo\func;
+
+class Category extends Model
+{
+    protected $fillable = ['name', 'is_directory', 'level', 'path'];
+    protected $casts = [
+        'is_directory' => 'boolean',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        // 监听 Category 的创建事件，用于初始化 path 和 level 字段值
+        static::creating(function (Category $category) {
+            // 如果创建的是一个根类目
+            if (is_null($category->parent_id)) {
+                // 将层级设为0
+                $category->level = 0;
+                // 将path设为-
+                $category->path = '-';
+            } else {
+                $category->level = $category->parent->level + 1;
+                $category->path  = $category->parent->path.$category->parent_id.'-';
+            }
+        });
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    // 获取所有祖先类目的 ID 值
+    public function getPathIdsAttribute()
+    {
+        return array_filter(explode('-', trim($this->path, '-')));
+    }
+
+    // 获取所有祖先类目，并按层级排序
+    public function getAncestorsAttribute()
+    {
+        return Category::query()
+            ->whereIn('id', $this->path_ids)
+            ->orderBy('level')
+            ->get();
+    }
+
+    // 获取以 - 为分割的所有祖先类目名称以及当前类目的名称
+    public function fetFullNameAttribute()
+    {
+        return $this->ancestors  // 获取所有祖先类目
+        ->pluck('name') // 取出所有祖先类目的 name 字段作为一个数组
+        ->push($this->name) // 将当前类目的 name 字段值加到数组的末尾
+        ->implode(' - ');
+    }
+}
