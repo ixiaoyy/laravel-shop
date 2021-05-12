ef35495371504ea41696de106cf9c578c9bc97b4
lijin <phpxiaoyz@163.com>
2021-05-12 18:58:21 +0800

    订单模块调整

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -11,6 +11,7 @@ use App\Exceptions\InvalidRequestException;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use App\Http\Requests\Admin\HandleRefundRequest;
 use App\Exceptions\InternalException;
+use App\Models\CrowdfundingProduct;
 
 class OrdersController extends AdminController
 {
@@ -71,7 +72,11 @@ class OrdersController extends AdminController
         if ($order->ship_status !== Order::SHIP_STATUS_PENDING) {
             throw new InvalidRequestException('该订单已发货');
         }
-        // Laravel 5.5 之后 validate 方法可以返回校验过的值
+        // 众筹订单只有在众筹成功之后发货
+        if ($order->type === Order::TYPE_CROWDFUNDING &&
+            $order->items[0]->product->crowdfunding->status !== CrowdfundingProduct::STATUS_SUCCESS) {
+            throw new InvalidRequestException('众筹订单只能在众筹成功之后发货');
+        }
         $data = $this->validate($request, [
             'express_company' => ['required'],
             'express_no'      => ['required'],
