97ec4b70c230ec259e02fc8031f968bc126a5226
yuy <phpxiaoyz@163.com>
2021-05-17 00:28:11 +0800

    创建分期付款

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -2,46 +2,44 @@
 
 namespace App\Http\Requests;
 
-use App\Models\CrowdfundingProduct;
-use App\Models\Product;
-use App\Models\ProductSku;
 use Illuminate\Validation\Rule;
+use App\Models\ProductSku;
 
-class CrowdFundingOrderRequest extends Request
+class OrderRequest extends Request
 {
     public function rules()
     {
         return [
-            'sku_id'     => [
+            // 判断用户提交的地址 ID 是否存在于数据库并且属于当前用户
+            // 后面这个条件非常重要，否则恶意用户可以用不同的地址 ID 不断提交订单来遍历出平台所有用户的收货地址
+            'address_id'     => [
+                'required',
+                Rule::exists('user_addresses', 'id')->where('user_id', $this->user()->id),
+            ],
+            'items'  => ['required', 'array'],
+            'items.*.sku_id' => [ // 检查 items 数组下每一个子数组的 sku_id 参数
                 'required',
                 function ($attribute, $value, $fail) {
                     if (!$sku = ProductSku::find($value)) {
                         return $fail('该商品不存在');
                     }
-                    // 众筹商品下单接口仅支持众筹商品的 SKU
-                    if ($sku->product->type !== Product::TYPE_CROWDFUNDING) {
-                        return $fail('该商品不支持众筹');
-                    }
                     if (!$sku->product->on_sale) {
                         return $fail('该商品未上架');
-                    }
-                    // 还需要判断众筹本身的状态，如果不是众筹中则无法下单
-                    if ($sku->product->crowdfunding->status !== CrowdfundingProduct::STATUS_FUNDING) {
-                        return $fail('该商品众筹已结束');
                     }
                     if ($sku->stock === 0) {
                         return $fail('该商品已售完');
                     }
-                    if ($this->input('amount') > 0 && $sku->stock < $this->input('amount')) {
+                    // 获取当前索引
+                    preg_match('/items\.(\d+)\.sku_id/', $attribute, $m);
+                    $index = $m[1];
+                    // 根据索引找到用户所提交的购买数量
+                    $amount = $this->input('items')[$index]['amount'];
+                    if ($amount > 0 && $amount > $sku->stock) {
                         return $fail('该商品库存不足');
                     }
                 },
             ],
-            'amount'     => ['required', 'integer', 'min:1'],
-            'address_id' => [
-                'required',
-                Rule::exists('user_addresses', 'id')->where('user_id', $this->user()->id),
-            ],
+            'items.*.amount' => ['required', 'integer', 'min:1'],
         ];
     }
 }
